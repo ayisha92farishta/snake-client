5bd6a7fc411f04534e5dcd497c61cc00270d2cf9
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-08-20 20:52:01 +0000

    gave our snake a name

--- client.js
+++ client.js
@@ -18,6 +18,14 @@ const connect = function () {
     console.log('Server says: ', data)
   })
 
+  //prints a message when connection is established
+
+  conn.on('connect', () => {
+    conn.write("Successfully connected to game server")
+    //gives our snake a name(i.e. AFA)
+    conn.write("Name: AFA")
+  })
+
   return conn;
 
 };
